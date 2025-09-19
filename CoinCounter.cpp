e5a4f68808f6d7854a52a02cfb744ff26110bcc9
tru0ngg <dangtru@bc.edu>
2025-09-19 16:07:54 -0400

    Update CoinCounter.cpp

--- CoinCounter.cpp
+++ CoinCounter.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 using namespace std;
 
@@ -34,15 +35,18 @@ int main(){
         }
         int total_cents = pennies + (nickels * 5) + (dimes * 10) + (quarters * 25);
         int dollars = total_cents / 100;
-        int cents = total_cents - (dollars * 100);
-        float total = dollars + (cents / 100.0);
-        if (dollars < 1){
+        int cents = total_cents % 100;
+        string total = to_string(dollars) + "." + to_string(cents);
+        if (dollars < 1 && cents == 1){
+                cout << "In total you have " << cents << " cent ($" << total << ")." << endl;
+        } else if (dollars < 1 && cents > 1) {
                 cout << "In total you have " << cents << " cents ($" << total << ")." << endl;
-        } else if (dollars == 1 && cents == 0){
-                cout << "In total you have " << dollars << " dollar and " << cents << " cents ($" << total << ".00)." << endl;
+        }
+        else if (dollars == 1 && cents == 0){
+                cout << "In total you have " << dollars << " dollar and " << cents << " cents ($" << total << ")." << endl;
         }
         else if (cents == 0){
-                cout << "In total you have " << dollars << " dollars and " << cents << " cents ($" << total << ".00)." << endl;
+                cout << "In total you have " << dollars << " dollars and " << cents << " cents ($" << total << ")." << endl;
         }
         else if (dollars == 1){
                 cout << "In total you have " << dollars << " dollar and " << cents << " cents ($" << total << ")." << endl;
