26ebb0d1b8f452e6eab8ff76bb58e189fe6836ee
tru0ngg <dangtru@bc.edu>
2025-09-19 15:28:13 -0400

    Update CoinCounter.cpp

--- CoinCounter.cpp
+++ CoinCounter.cpp
@@ -2,24 +2,40 @@
 using namespace std;
 
 int main(){
-        int pennies;
-        int nickels;
-        int dimes;
-        int quarters;
+        int pennies, nickels, dimes, quarters;
 
         cout << "How many pennies, nickels, dimes, and quarters do you have? " << endl;
         cin >> pennies >> nickels >> dimes >> quarters;
-        cout << "You have ";
-        cout << pennies << (pennies == 1 ? " penny" : " pennies") << ", ";
-        cout << nickels << (nickels == 1 ? " nickel" : " nickels") << ", ";
-        cout << dimes << (dimes == 1 ? " dime" : " dimes") << ", and ";
-        cout << quarters << (quarters == 1 ? " quarter" : " quarters") << "." << endl;
-        
+        if (pennies == 1){
+                cout << "You have 1 penny, ";
+        }
+        else {
+                cout << "You have " << pennies << " pennies, ";
+        }
+
+        if (nickels == 1){
+                cout << "1 nickel, ";
+        } else {
+                cout << nickels << " nickels, ";
+        }
+
+        if (dimes == 1){
+                cout <<"1 dime, and ";
+        }
+        else {
+                cout << dimes << " dimes, and ";
+        }
+
+        if (quarters == 1){
+                cout <<"1 quarter. " << endl;
+        }
+        else {
+                cout << quarters << " quarters." << endl;
+        }
         int total_cents = pennies + (nickels * 5) + (dimes * 10) + (quarters * 25);
         int dollars = total_cents / 100;
         int cents = total_cents - (dollars * 100);
         float total = dollars + (cents / 100.0);
-        
         if (dollars < 1){
                 cout << "In total you have " << cents << " cents ($" << total << ")." << endl;
         } else if (dollars == 1 && cents == 0){
@@ -29,7 +45,7 @@ int main(){
                 cout << "In total you have " << dollars << " dollars and " << cents << " cents ($" << total << ".00)." << endl;
         }
         else if (dollars == 1){
-                cout << "In total you have " << dollars << " dollar and " << cents << " cents ($" << total << ")." << endl;
+                cout << "In total you have " << dollars << " dollar and " << cents << " cents ($" << total << ".)." << endl;
         }
         else {
                 cout << "In total you have " << dollars << " dollars and " << cents << " cents ($" << total << ")." << endl;
