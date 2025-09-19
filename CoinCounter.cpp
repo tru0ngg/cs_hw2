56ee73273d2c8774ae05f8e8b36e159906c0c06a
tru0ngg <dangtru@bc.edu>
2025-09-19 17:11:07 -0400

    Update CoinCounter.cpp

--- CoinCounter.cpp
+++ CoinCounter.cpp
@@ -7,9 +7,6 @@ int main(){
 
         cout << "How many pennies, nickels, dimes, and quarters do you have?" << endl;
         cin >> pennies >> nickels >> dimes >> quarters;
-        if (pennies < 0 || pennies > 100 || nickels < 0 || nickels > 100 || dimes < 0 || dimes > 100 || quarters < 0 || quarters > 100) {
-                return 0;
-        }
         if (pennies == 1){
                 cout << "You have 1 penny, ";
         }
@@ -42,11 +39,11 @@ int main(){
         int cents = total_cents % 100;
         if (dollars == 0 && cents == 1){
                 cout << "In total you have 1 cent ($0.01)." << endl;
-        } else if (dollars == 0 && cents != 1) {
+        } else if (dollars == 0) {
                 cout << "In total you have " << cents << " cents ($" << fixed << setprecision(2) << (total_cents / 100.0) << ")." << endl;
         } else if (dollars == 1 && cents == 1){
                 cout << "In total you have 1 dollar and 1 cent ($1.01)." << endl;
-        } else if (dollars == 1 && cents != 1){
+        } else if (dollars == 1){
                 cout << "In total you have 1 dollar and " << cents << " cents ($"  << fixed << setprecision(2) << (total_cents / 100.0) << ")." << endl;
         }
         else if (cents == 1) {
