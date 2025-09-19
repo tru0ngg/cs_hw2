9710a8c74596979db04cbaff8780dffb9308ab68
tru0ngg <dangtru@bc.edu>
2025-09-19 16:36:25 -0400

    Update CoinCounter.cpp

--- CoinCounter.cpp
+++ CoinCounter.cpp
@@ -1,5 +1,5 @@
-
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
@@ -35,24 +35,19 @@ int main(){
         }
         int total_cents = pennies + (nickels * 5) + (dimes * 10) + (quarters * 25);
         int dollars = total_cents / 100;
+
         int cents = total_cents % 100;
-        string total = to_string(dollars) + "." + to_string(cents);
-        if (dollars < 1 && cents == 1){
-                cout << "In total you have " << cents << " cent ($" << total << ")." << endl;
-        } else if (dollars < 1 && cents > 1) {
-                cout << "In total you have " << cents << " cents ($" << total << ")." << endl;
-        }
-        else if (dollars == 1 && cents == 0){
-                cout << "In total you have " << dollars << " dollar and " << cents << " cents ($" << total << ")." << endl;
-        }
-        else if (cents == 0){
-                cout << "In total you have " << dollars << " dollars and " << cents << " cents ($" << total << ")." << endl;
-        }
-        else if (dollars == 1){
-                cout << "In total you have " << dollars << " dollar and " << cents << " cents ($" << total << ")." << endl;
+        if (dollars == 0 && cents == 1){
+                cout << "In total you have " << cents << " cent ($0.01)." << endl;
+        } else if (dollars == 0) {
+                cout << "In total you have " << cents << " cents ($" << fixed << setprecision(2) << (total_cents / 100.0) << ")." << endl;
+        } else if (dollars == 1 && cents == 1){
+                cout << "In total you have 1 dollar and 1 cent ($1.01)." << endl;
+        } else if (dollars == 1){
+                cout << "In total you have 1 dollar and " << cents << " cents ($"  << fixed << setprecision(2) << (total_cents / 100.0) << ")." << endl;
         }
         else {
-                cout << "In total you have " << dollars << " dollars and " << cents << " cents ($" << total << ")." << endl;
+                cout << "In total you have " << dollars << " dollars and " << cents << " cents ($" << fixed << setprecision(2) << (total_cents / 100.0) << ")." << endl;
         }
         return 0;
 }
